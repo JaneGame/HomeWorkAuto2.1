1bbeb399090486cc7699f8f22a5edf046257aaed
JaneGame <fibi92@mail.ru>
2021-10-24 18:35:43 +0300

    SpecialError

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,6 +3,8 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
@@ -10,12 +12,14 @@ class MobileBankApiTestV1 {
       // Given - When - Then
       // Предусловия
       given()
-          .baseUri("http://localhost:9999/api/v1")
+          .baseUri("http://0.0.0.0:9999/api/v1")
       // Выполняемые действия
       .when()
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+          .body("[0].currency", equalTo("RUR"));
     }
 }
